02d94d387a26e1dac5e6486d46676d08568cc7c7
Rogerz Zhang <rogerz.zhang@gmail.com>
2016-03-09 20:54:12 +0800

    feat: update template

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
+ruby "2.2.3"
+
 # Specify your gem's dependencies in yuntongxun.gemspec
 gemspec
 
-gem 'webmock', '1.24.1'
 gem 'dotenv', '2.1.0'
--- spec/yuntongxun_spec.rb
+++ spec/yuntongxun_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
 
 describe YunTongXun do
-  it 'has a version number' do
-    expect(YunTongXun::VERSION).not_to be nil
-  end
-
-  it 'send sms' do
-    ytx = YunTongXun::RestClient.new(
+  before do
+    @ytx = YunTongXun::RestClient.new(
         api: ENV['YTX_API_SERVER'],
         version: '2013-12-26',
         account_sid: ENV['YTX_ACCOUNT_SID'],
         auth_token: ENV['YTX_AUTH_TOKEN']
       )
-    ytx.config(app_id: ENV['YTX_APP_ID'], template_id: ENV['YTX_SMS_TEMPLATE'])
-    response = ytx.send_sms(to: ENV['YTX_TEST_TO'], data: ['2016', 1])
+  end
+  it 'has a version number' do
+    expect(YunTongXun::VERSION).not_to be nil
+  end
+
+  it 'send token' do
+    @ytx.config(app_id: ENV['YTX_APP_ID'], template_id: ENV['YTX_TEMPLATE_FOR_TOKEN'])
+    response = @ytx.send_sms(to: ENV['YTX_TEST_TO'], data: ['2016', 1])
+    expect(JSON.parse(response.body)['statusCode']).to eql '000000'
+  end
+
+  it 'send welcome' do
+    @ytx.config(app_id: ENV['YTX_APP_ID'], template_id: ENV['YTX_TEMPLATE_FOR_WELCOME'])
+    response = @ytx.send_sms(to: ENV['YTX_TEST_TO'], data: [])
     expect(JSON.parse(response.body)['statusCode']).to eql '000000'
   end
 end
